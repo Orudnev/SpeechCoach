908083d4dc0333e60ec8e92063558ff817ba91c5
Orudnev <olrudnev@gmail.com>
2022-12-21 18:31:11 +0600

    1-st commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Dictaphone from './Dictaphone';
+
+function App() {
+  return (
+    <div className="App">
+      <Dictaphone />
+    </div>
+  );
+}
+
+export default App;
